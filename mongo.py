19107ab8bd17979c9a2118f0b495fc40bee57a93
Muhammed Nuhman <nuhman7@outlook.com>
2018-05-18 11:15:23 +0530

    add comments

--- mongo.py
+++ mongo.py
@@ -1,3 +1,7 @@
+"""
+MongoDB setup - 'mlab.com'.
+"""
+
 from pymongo import MongoClient
 from datetime import datetime
 import os
@@ -5,14 +9,18 @@ import os
 db_url = 'mongodb://{}:{}@ds111410.mlab.com:11410/iot'.format(
     os.environ.get('MONGO_USERNAME'), os.environ.get('MONGO_PASSWORD'))
 
+# set up connection to MongoDB client
 client = MongoClient(db_url)
 
+# In 'iot' db there is a mongo collection called 'user_db'
 user_db = client.iot.get_collection('user_db')
 
 
 def add_user(user):
-  count = user_db.count()
+  count = user_db.count() # get the current count
   if not user_db.find_one({'user_id': user.get('user_id')}):
+    # if the 'user' doesn't exist in the collection yet, 
+    # then add the user to the collection
     user['id'] = count + 1
     user_db.insert_one(user)
 
